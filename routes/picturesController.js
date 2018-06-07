a6937d29ec38bb10e020fa3a5af0229f1252ff14
jcj0002 <jcj0002@gmail.com>
2018-06-07 18:24:10 -0400

    create route for pictures

--- routes/picturesController.js
+++ routes/picturesController.js
@@ -14,13 +14,29 @@ router.get('/', (req, res, next) => {
             // Once you have all pictures, then render out index page 
             res.render('pictures/index', { groupOfPictures: groupOfPictures })
         })
-        .catch((err)=> res.send(err))
+        .catch((err) => res.send(err))
 
 })
 
 //NEW Route
-router.get('/new', (req,res)=> {
+router.get('/new', (req, res) => {
     res.render('pictures/new')
 })
 //CREATE Route
+router.post('/', (req, res) => {
+    const newPicture = req.body
+    Pictures
+        .create(newPicture)
+        .then(() => {
+            res.redirect('/pictures')
+        })
+})
+//SHOW ROUTE
+router.get('/:id', (req, res) => {
 
+    Pictures
+    
+
+})
+
+module.exports = router
