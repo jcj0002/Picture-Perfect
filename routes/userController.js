bb6c3fbff77de4e07d2547e6b5180b37bebd8220
jcj0002 <jcj0002@gmail.com>
2018-06-10 17:59:27 -0400

    completed delete route for users

--- routes/userController.js
+++ routes/userController.js
@@ -21,21 +21,22 @@ router.get('/', (req, res, next) => {
 //NEW Route
 router.get('/new', (req, res) => {
   res.render('user/new')
-  
+  console.log ("new is working")
 })
-console.log ("new is working")
+
 //CREATE Route
 router.post('/', (req, res) => {
   const newUser = req.body
   User
     .create(newUser)
     .then(() => {
+      console.log ("create is working")
       res.redirect('/user')
-     
+      console.log ("create is working")
     })
     
 })
-console.log ("create is working")
+
 
 //SHOW ROUTE
 router.get('/:id', (req, res) => {
@@ -43,25 +44,27 @@ router.get('/:id', (req, res) => {
   User
       .findById(req.params.id)
       .then((individualUser) => {
+        console.log ("show is working")
           res.render('user/show', { individualUser })
      
         })
 
 })
 
-console.log ("show is working")
+
 
 //EDIT ROUTE
 router.get('/:id/edit', (req, res) => {
   User
       .findById(req.params.id)
       .then((users) => {
+        console.log ("edit is working")
           res.render('user/edit', { individualUser: users })
           
         })
       
 })
-console.log ("edit is working")
+
 
 // UPDATE Route
 router.put('/:id', (req, res) => {
@@ -70,6 +73,16 @@ router.put('/:id', (req, res) => {
 
   })
 })
+
+//DELETE Route
+router.delete('/:id', (req, res)=> {
+  User.findByIdAndRemove(req.params.id)
+  .then(()=>{
+    console.log('Delete is working')
+    res.redirect('/user')
+  })
+})
+
 
 
 module.exports = router
